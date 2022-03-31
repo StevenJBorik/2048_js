08ddde9babae64f1bddce2787114671740ea7459
Steven Borblob <stevenjborik@protonmail.com>
2022-03-31 17:19:58 -0500

    Tile class, movement functions

--- Grid.js
+++ Grid.js
@@ -1,33 +1,55 @@
 const GRID_SIZE = 4
 const CELL_SIZE = 20 
 const CELL_GAP = 2 
-const BORDER_RAD = 1
+
 
 export default class Grid{
     #cells 
-    #cellElement
-    #x
-    #y
+   
 
     constructor(gridElement) {
         gridElement.style.setProperty("--grid-size", GRID_SIZE)
         gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
         gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
-        gridElement.style.setProperty("--border-rad", `${BORDER_RAD}vmin`)
         createCellElements(gridElement)
         this.#cells = createCellElements(gridElement).map((cellElement, index) => {
             return new Cell(cellElement, Math.floor(index % GRID_SIZE, index / GRID_SIZE))
         })
     }
 
+    get #emptyCells() {
+        return this.#cells.filter(cell => cell.tile == null)
+    }
+
+    randomEmptyCell() {
+        const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
+        return this.#emptyCells[randomIndex]
+    }
+
     
 }
 
 class Cell {
+    #cellElement
+    #x
+    #y
+    #tile
+
     constructor(cellElement, x, y) {
         this.#cellElement = cellElement
         this.#x = x
         this.#y = y 
+    }
+
+    get tile() {
+        return this.#tile
+    }
+
+    set tile(value) {
+        this.#tile = value
+        if (value == null) return
+        this.#tile.x = this.#x
+        this.#tile.y = this.#y
     }
 }
 
